975edee0e20342ea703134ce82dc109e00444969
Carolina Acosta <carolinaacosta.nuez@gmail.com>
2020-05-03 18:05:43 +0100

    added rotate functions

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -14,32 +14,39 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null)
   const [gameOver, setGameOver] = useState(false)
 
-  const [player, updatePlayerPos, resetPlayer] = usePlayer()
-  const [stage, setStage] = useStage(player)
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer()
+  const [stage, setStage] = useStage(player, resetPlayer)
+
   console.log('re-render')
+
   const movePlayer = (dir) => {
     if (!checkCollision(player, stage, { x: dir, y: 0 })) {
       updatePlayerPos({ x: dir, y: 0 })
     }
   }
+
   const startGame = () => {
-    //reset everything
+    console.log('test')
+    // Reset everything
     setStage(createStage())
     resetPlayer()
     setGameOver(false)
   }
+
   const drop = () => {
     if (!checkCollision(player, stage, { x: 0, y: 1 })) {
       updatePlayerPos({ x: 0, y: 1, collided: false })
     } else {
+      // Game Over
       if (player.pos.y < 1) {
-        console.log('gameover')
+        console.log('GAME OVER!!!')
         setGameOver(true)
         setDropTime(null)
       }
       updatePlayerPos({ x: 0, y: 0, collided: true })
     }
   }
+
   const dropPlayer = () => {
     drop()
   }
@@ -52,6 +59,8 @@ const Tetris = () => {
         movePlayer(1)
       } else if (keyCode === 40) {
         dropPlayer()
+      } else if (keyCode === 38) {
+        playerRotate(stage, 1)
       }
     }
   }
