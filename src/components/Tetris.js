f8c31115eb8f2717a0ce6a32cec08967a2630bf0
Carolina Acosta <carolinaacosta.nuez@gmail.com>
2020-05-03 14:45:01 +0100

    adding collision

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { createStage } from '../gameHelpers'
+import { createStage, checkCollision } from '../gameHelpers'
 //styled Components
 import { StyledTetrisWrapper, StyledTetris } from './styles/StyledTetris'
 // Custom Hooks
@@ -18,15 +18,27 @@ const Tetris = () => {
   const [stage, setStage] = useStage(player)
   console.log('re-render')
   const movePlayer = (dir) => {
-    updatePlayerPos({ x: dir, y: 0 })
+    if (!checkCollision(player, stage, { x: dir, y: 0 })) {
+      updatePlayerPos({ x: dir, y: 0 })
+    }
   }
   const startGame = () => {
     //reset everything
     setStage(createStage())
     resetPlayer()
+    setGameOver(false)
   }
   const drop = () => {
-    updatePlayerPos({ x: 0, y: 1, collided: false })
+    if (!checkCollision(player, stage, { x: 0, y: 1 })) {
+      updatePlayerPos({ x: 0, y: 1, collided: false })
+    } else {
+      if (player.pos.y < 1) {
+        console.log('gameover')
+        setGameOver(true)
+        setDropTime(null)
+      }
+      updatePlayerPos({ x: 0, y: 0, collided: true })
+    }
   }
   const dropPlayer = () => {
     drop()
