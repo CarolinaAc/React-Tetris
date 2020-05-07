e97f80ae318ad2a5ce27724acec9d9ff0e5a79d2
Carolina Acosta <carolinaacosta.nuez@gmail.com>
2020-05-07 19:09:11 +0100

    Updates

---
+++ precache-manifest.0a046d67df0b670bb2b5c6c79d5c0e05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93662b3f145883f80c8314a59bdf0cab",
+    "url": "/React-Tetris/index.html"
+  },
+  {
+    "revision": "df339d0285dc62c2779e",
+    "url": "/React-Tetris/static/css/main.2504560d.chunk.css"
+  },
+  {
+    "revision": "d891cd67ac97deedf3e8",
+    "url": "/React-Tetris/static/js/2.09561226.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Tetris/static/js/2.09561226.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df339d0285dc62c2779e",
+    "url": "/React-Tetris/static/js/main.e1334af2.chunk.js"
+  },
+  {
+    "revision": "63a8a2d479034e9ba1c0",
+    "url": "/React-Tetris/static/js/runtime-main.c16276df.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/React-Tetris/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/React-Tetris/static/media/bg.2810fe6b.png"
+  }
+]);
